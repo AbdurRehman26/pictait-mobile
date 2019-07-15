f0a74078e262588b69393975da6cf11f3ec0f791
Kazmi <abdur.rehman@startrum.com>
2019-07-15 17:56:17 +0500

    facebook setup done

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Text, TouchableOpacity} from 'react-native';
+var { FBLogin, FBLoginManager } = require('react-native-facebook-login');
 
 class Login extends Component{
   login(){
@@ -7,7 +8,7 @@ class Login extends Component{
   }
 
   render(){
-
+    var _this = this;
     return (
     <TouchableOpacity
     onPress={()=>{
@@ -20,8 +21,43 @@ class Login extends Component{
       justifyContent : 'center',
       alignItems : 'center'
     }}
-    >
-        <Text>Login</Text>
+    > 
+
+<FBLogin style={{ marginBottom: 10, }}
+        ref={(fbLogin) => { this.fbLogin = fbLogin }}
+        permissions={["email","user_friends"]}
+        loginBehavior={FBLoginManager.LoginBehaviors.Native}
+        onLogin={function(data){
+          console.log("Logged in!");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLogout={function(){
+          console.log("Logged out.");
+          _this.setState({ user : null });
+        }}
+        onLoginFound={function(data){
+          console.log("Existing login found.");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLoginNotFound={function(){
+          console.log("No user logged in.");
+          _this.setState({ user : null });
+        }}
+        onError={function(data){
+          console.log("ERROR");
+          console.log(data);
+        }}
+        onCancel={function(){
+          console.log("User cancelled.");
+        }}
+        onPermissionsMissing={function(data){
+          console.log("Check permissions!");
+          console.log(data);
+        }}
+      />
+
     </TouchableOpacity>
   );
 }
